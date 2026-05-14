a0a48d676830896101e3b51bdb46ed6ed7c5d0ea
abboo7 <abbootoo7@gmail.com>
2026-05-14 02:30:30 -0400

    fix layout

--- src/components/OwnerLayout.jsx
+++ src/components/OwnerLayout.jsx
@@ -1,21 +1,17 @@
-import OwnerSidebar from './OwnerSidebar';
-import OwnerHeader from './OwnerHeader';
-import VerificationBanner from './VerificationBanner';
+import OwnerSidebar from '@/features/owners/components/OwnerSidebar';
+import OwnerHeader from '@/features/owners/components/OwnerHeader';
 import { Outlet } from 'react-router';
 
-function OwnerLayout() {
+export default function OwnerLayout() {
   return (
-    <div className="bg-background font-display text-foreground flex min-h-screen">
+    <div className="bg-background flex min-h-screen">
       <OwnerSidebar />
-      <main className="ml-72 flex-1">
+      <div className="flex flex-1 flex-col">
         <OwnerHeader />
-        <div className="px-8 pt-2">
-          <VerificationBanner verificationState="pending_documents" />
-        </div>
-        <Outlet />
-      </main>
+        <main className="bg-muted/30 flex-1 p-6">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
-
-export default OwnerLayout;
